6938ed5228aed312eb86859ab7c2100c99b7bb48
Sumaiya Tamboli <stamboli@adobe.com>
2026-03-05 13:09:34 +0530

    cars changed

---
+++ blocks/cars/cars.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const carCard = document.querySelector(".car.block");
+
+  carCard.addEventListener("click", function () {
+    alert("You clicked Jaguar car!");
+  });
+
+});
